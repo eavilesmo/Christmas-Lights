91f0a929b132117e3397569bf8d3a15042b6a2da
Esperanza <esperanza.aviles@clarity.ai>
2022-09-02 18:46:22 +0200

    refactor: refactor test code

--- src/test/java/GridTest.java
+++ src/test/java/GridTest.java
@@ -4,14 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class GridTest {
 
-//    @Test
-//    void check_grid_is_created() {
-//        Grid grid = new Grid();
-//        int[][] actualGrid = grid.create(10, 10);
-//        int[][] expectedGrid = new int[10][10];
-//        assertThat(actualGrid).isEqualTo(expectedGrid);
-//    }
-
     @Test
     void check_grid_is_filled_with_lights() {
         Grid grid = new Grid();
